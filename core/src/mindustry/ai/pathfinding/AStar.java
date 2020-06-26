007e0e1a3e5fa0f1231266c3cc2592bee70f1447
Max Leibowitz <403089@bsd48.org>
2020-06-25 18:44:02 -0700

    Added AStar thing, hopefully works

---
+++ core/src/mindustry/ai/pathfinding/AStar.java
@@ -0,0 +1,356 @@
+package mindustry.ai.pathfinding;
+
+import arc.math.*;
+import arc.struct.*;
+import mindustry.game.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.defense.turrets.Turret.*;
+
+import java.util.*;
+
+
+// Taken from http://www.codebytes.in/2015/02/a-shortest-path-finding-algorithm.html
+// and modified
+
+
+public class AStar{
+    public static final int DIAGONAL_COST = 14;
+    public static final int V_H_COST = 10;
+
+    static class Cell{
+        int heuristicCost = 0; //Heuristic cost
+        int finalCost = 0; //G+H
+        int i, j;
+        Cell parent;
+
+        Cell(int i, int j){
+            this.i = i;
+            this.j = j;
+        }
+
+        @Override
+        public String toString(){
+            return "[" + this.i + ", " + this.j + "]";
+        }
+    }
+
+    //Blocked cells are just null Cell values in grid
+    static Cell[][] grid = new Cell[5][5];
+
+    static PriorityQueue<Cell> open = new PriorityQueue<>();
+
+    static boolean closed[][];
+    static int startI, startJ;
+    static int endI, endJ;
+
+    public static void setBlocked(int i, int j){
+        grid[i][j] = null;
+    }
+
+    public static void setStartCell(int i, int j){
+        startI = i;
+        startJ = j;
+    }
+
+    public static void setEndCell(int i, int j){
+        endI = i;
+        endJ = j;
+    }
+
+    static void checkAndUpdateCost(Cell current, Cell t, int cost){
+        if(t == null || closed[t.i][t.j]) return;
+        int t_final_cost = t.heuristicCost + cost;
+
+        boolean inOpen = open.contains(t);
+        if(!inOpen || t_final_cost < t.finalCost){
+            t.finalCost = t_final_cost;
+            t.parent = current;
+            if(!inOpen) open.add(t);
+        }
+    }
+
+    public static void AStar(){
+
+        //add the start location to open list.
+        open.add(grid[startI][startJ]);
+
+        Cell current;
+
+        while(true){
+            current = open.poll();
+            if(current == null) break;
+            closed[current.i][current.j] = true;
+
+            if(current.equals(grid[endI][endJ])){
+                return;
+            }
+
+            Cell t;
+            if(current.i - 1 >= 0){
+                t = grid[current.i - 1][current.j];
+                checkAndUpdateCost(current, t, current.finalCost + V_H_COST);
+
+                if(current.j - 1 >= 0){
+                    t = grid[current.i - 1][current.j - 1];
+                    checkAndUpdateCost(current, t, current.finalCost + DIAGONAL_COST);
+                }
+
+                if(current.j + 1 < grid[0].length){
+                    t = grid[current.i - 1][current.j + 1];
+                    checkAndUpdateCost(current, t, current.finalCost + DIAGONAL_COST);
+                }
+            }
+
+            if(current.j - 1 >= 0){
+                t = grid[current.i][current.j - 1];
+                checkAndUpdateCost(current, t, current.finalCost + V_H_COST);
+            }
+
+            if(current.j + 1 < grid[0].length){
+                t = grid[current.i][current.j + 1];
+                checkAndUpdateCost(current, t, current.finalCost + V_H_COST);
+            }
+
+            if(current.i + 1 < grid.length){
+                t = grid[current.i + 1][current.j];
+                checkAndUpdateCost(current, t, current.finalCost + V_H_COST);
+
+                if(current.j - 1 >= 0){
+                    t = grid[current.i + 1][current.j - 1];
+                    checkAndUpdateCost(current, t, current.finalCost + DIAGONAL_COST);
+                }
+
+                if(current.j + 1 < grid[0].length){
+                    t = grid[current.i + 1][current.j + 1];
+                    checkAndUpdateCost(current, t, current.finalCost + DIAGONAL_COST);
+                }
+            }
+        }
+    }
+
+    /*
+    Params :
+    tCase = test case No.
+    x, y = Board's dimensions
+    si, sj = start location's x and y coordinates
+    ei, ej = end location's x and y coordinates
+    int[][] blocked = array containing inaccessible cell coordinates
+    */
+    public static void test(int tCase, int x, int y, int si, int sj, int ei, int ej, int[][] blocked){
+        System.out.println("\n\nTest Case #" + tCase);
+        //Reset
+        grid = new Cell[x][y];
+        closed = new boolean[x][y];
+        open = new PriorityQueue<>((Object o1, Object o2) -> {
+            Cell c1 = (Cell)o1;
+            Cell c2 = (Cell)o2;
+
+            return c1.finalCost < c2.finalCost ? -1 :
+            c1.finalCost > c2.finalCost ? 1 : 0;
+        });
+        //Set start position
+        setStartCell(si, sj);  //Setting to 0,0 by default. Will be useful for the UI part
+
+        //Set End Location
+        setEndCell(ei, ej);
+
+        for(int i = 0; i < x; ++i){
+            for(int j = 0; j < y; ++j){
+                grid[i][j] = new Cell(i, j);
+                grid[i][j].heuristicCost = Math.abs(i - endI) + Math.abs(j - endJ);
+//                  System.out.print(grid[i][j].heuristicCost+" ");
+            }
+//              System.out.println();
+        }
+        grid[si][sj].finalCost = 0;
+
+           /*
+             Set blocked cells. Simply set the cell values to null
+             for blocked cells.
+           */
+        for(int i = 0; i < blocked.length; ++i){
+            setBlocked(blocked[i][0], blocked[i][1]);
+        }
+
+        //Display initial map
+        System.out.println("Grid: ");
+        for(int i = 0; i < x; ++i){
+            for(int j = 0; j < y; ++j){
+                if(i == si && j == sj) System.out.print("SO  "); //Source
+                else if(i == ei && j == ej) System.out.print("DE  ");  //Destination
+                else if(grid[i][j] != null) System.out.printf("%-3d ", 0);
+                else System.out.print("BL  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        AStar();
+        System.out.println("\nScores for cells: ");
+        for(int i = 0; i < x; ++i){
+            for(int j = 0; j < x; ++j){
+                if(grid[i][j] != null) System.out.printf("%-3d ", grid[i][j].finalCost);
+                else System.out.print("BL  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        if(closed[endI][endJ]){
+            //Trace back the path
+            System.out.println("Path: ");
+            Cell current = grid[endI][endJ];
+            System.out.print(current);
+            while(current.parent != null){
+                System.out.print(" -> " + current.parent);
+                current = current.parent;
+            }
+            System.out.println();
+        }else System.out.println("No possible path");
+    }
+
+    public static Array<int[]> findPathTurrets(Array<TurretEntity> turrets, float playerX, float playerY, float targetX, float targetY, int width, int height, Team team){
+        Array<TurretPathfindingEntity> pathfindingEntities = new Array<>();
+        for(TurretEntity turretEntity : turrets){
+            if(turretEntity.getTeam() == team){
+                continue;
+            }
+            pathfindingEntities.add(new TurretPathfindingEntity(turretEntity.tileX(), turretEntity.tileY(), ((Turret)turretEntity.block).range));
+        }
+        return findPath(pathfindingEntities, playerX, playerY, targetX, targetY, width, height);
+    }
+
+    public static Array<int[]> findPath(Array<TurretPathfindingEntity> turrets, float playerX, float playerY, float targetX, float targetY, int width, int height){
+        ArrayList<int[]> blocked2 = new ArrayList<>();
+        for(TurretPathfindingEntity turret : turrets){
+//            if(turret.getTeam() == player.getTeam()){
+//                continue;
+//            }
+            float range = turret.range;
+            float x = turret.x;
+            float y = turret.y;
+            int count = 0;
+            for(int colNum = 0; colNum <= width - 1; colNum += 1){
+                if(colNum > x + range){
+                    continue;
+                }
+                if(colNum < x - range){
+                    continue;
+                }
+
+                for(int blockNum = 0; blockNum <= height - 1; blockNum += 1){
+                    if(blockNum > y + range){
+                        continue;
+                    }
+                    if(blockNum < y - range){
+                        continue;
+                    }
+
+                    if(Mathf.sqrt(Mathf.pow(x - colNum, 2) + Mathf.pow(y - blockNum, 2)) < range){
+                        blocked2.add(new int[]{colNum, blockNum});
+                        count += 1;
+                    }
+                }
+            }
+        }
+        int[][] blocked = new int[blocked2.size()][2];
+        for(int b = 0; b <= blocked2.size() - 1; b += 1){
+            blocked[b] = blocked2.get(b);
+        }
+        //Reset
+        int px = (int)playerX / 8;
+        int py = (int)playerY / 8;
+
+        int ex = (int)targetX / 8;
+        int ey = (int)targetY / 8;
+        grid = new Cell[width][height];
+        closed = new boolean[width][height];
+        open = new PriorityQueue<>((Object o1, Object o2) -> {
+            Cell c1 = (Cell)o1;
+            Cell c2 = (Cell)o2;
+
+            return Integer.compare(c1.finalCost, c2.finalCost);
+        });
+        //Set start position
+        setStartCell(px, py);  //Setting to 0,0 by default. Will be useful for the UI part
+
+        //Set End Location
+        setEndCell(ex, ey);
+
+        for(int i = 0; i < width; ++i){
+            for(int j = 0; j < height; ++j){
+                grid[i][j] = new Cell(i, j);
+                grid[i][j].heuristicCost = Math.abs(i - endI) + Math.abs(j - endJ);
+//                  System.out.print(grid[i][j].heuristicCost+" ");
+            }
+//              System.out.println();
+        }
+        grid[px][py].finalCost = 0;
+
+           /*
+             Set blocked cells. Simply set the cell values to null
+             for blocked cells.
+           */
+        for(int i = 0; i < blocked.length; ++i){
+            setBlocked(blocked[i][0], blocked[i][1]);
+        }
+
+        //Display initial map
+//        System.out.println("Grid: ");
+//        for(int i = 0; i < width; ++i){
+//            for(int j = 0; j < height; ++j){
+//                if(i == px && j == py) System.out.print("SO  "); //Source
+//                else if(i == ex && j == ey) System.out.print("DE  ");  //Destination
+//                else if(grid[i][j] != null) System.out.printf("%-3d ", 0);
+//                else System.out.print("BL  ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+
+        AStar();
+//        System.out.println("\nScores for cells: ");
+//        for(int i = 0; i < width; ++i){
+//            for(int j = 0; j < height; ++j){
+//                if(grid[i][j] != null) System.out.printf("%-3d ", grid[i][j].finalCost);
+//                else System.out.print("BL  ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+
+        if(closed[endI][endJ]){
+            Array<int[]> points = new Array<>();
+            //Trace back the path
+//            System.out.println("Path: ");
+            Cell current = grid[endI][endJ];
+            System.out.print(current);
+            while(current.parent != null){
+//                System.out.print(" -> " + current.parent);
+                points.add(new int[]{current.parent.i, current.parent.j});
+                current = current.parent;
+            }
+            return points;
+//            System.out.println();
+        }else{
+            return null;
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception{
+//        test(1, 5, 5, 0, 0, 3, 2, new int[][]{{0,4},{2,2},{3,1},{3,3}});
+//        test(2, 5, 5, 0, 0, 4, 4, new int[][]{{0,4},{2,2},{3,1},{3,3}});
+//        test(3, 7, 7, 2, 1, 5, 4, new int[][]{{4,1},{4,3},{5,3},{2,3}});
+//
+//        test(1, 5, 5, 0, 0, 4, 4, new int[][]{{3,4},{3,3},{4,3}});
+        Random r = new Random();
+        Array<TurretPathfindingEntity> turrets = new Array<>();
+        for(int i = 0; i < 10; i += 1){
+            int x = (int)(r.nextFloat() * 50);
+            int y = (int)(r.nextFloat() * 50);
+            turrets.add(new TurretPathfindingEntity(x, y, 5f));
+        }
+        findPath(turrets, 8f, 16f, 32f * 8, 48f * 8, 50, 50);
+    }
+}
---
+++ core/src/mindustry/ai/pathfinding/PlayerPathfinder.java
@@ -0,0 +1,14 @@
+package mindustry.ai.pathfinding;
+
+import arc.struct.*;
+import mindustry.game.*;
+import mindustry.world.blocks.defense.turrets.*;
+
+public class PlayerPathfinder{
+
+    public static void findPath(Array<Turret> turrets){
+        for(Turret t : turrets){
+
+        }
+    }
+}
