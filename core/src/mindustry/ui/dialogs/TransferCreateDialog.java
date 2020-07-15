9aaf44c29f2e1c75e0c521ee294e7766ae60e243
Max Leibowitz <403089@bsd48.org>
2020-07-15 10:29:48 -0700

    Working on it, cleaner now

--- core/src/mindustry/ui/dialogs/TransferCreateDialog.java
+++ core/src/mindustry/ui/dialogs/TransferCreateDialog.java
@@ -9,6 +9,9 @@ import mindustry.game.*;
 import mindustry.gen.*;
 import mindustry.ui.*;
 import mindustry.world.*;
+import mindustry.world.blocks.*;
+
+import java.util.concurrent.atomic.*;
 
 public class TransferCreateDialog extends FloatingDialog{
     private TransferEndpoint start;
@@ -19,46 +22,67 @@ public class TransferCreateDialog extends FloatingDialog{
         build();
     }
 
-    public void build(){
-        Table typeSelector = new Table();
-        typeSelector.addButton("Single tile", () -> {
-            cont.removeChild(typeSelector);
-            TextField x = new TextField("0");
-            x.setValidator((string) -> Strings.canParsePostiveInt(string) && Integer.parseInt(string) < Vars.world.width());
-            TextField y = new TextField("0");
-            y.setValidator((string) -> Strings.canParsePostiveInt(string) && Integer.parseInt(string) < Vars.world.height());
-            Label comma = new Label(",");
-            cont.add(x, comma, y);
-            Image blockImage = new Image(Icon.block);
-            blockImage.update(() -> {
-                if(x.isValid() && y.isValid()){
-                    Tile tile = Vars.world.tile(Integer.parseInt(x.getText()), Integer.parseInt(y.getText()));
-                    if(tile.block() == Blocks.air){
-                        blockImage.setDrawable(tile.floor().icon(Cicon.small));
-                    }else{
-                        blockImage.setDrawable(tile.block().icon(Cicon.small));
-                    }
+    private Table buildSingleTilePickup(boolean isStart){
+        Table table = new Table();
+        TextField x = new TextField("0");
+        TextField y = new TextField("0");
+        x.setValidator((string) -> Strings.canParsePostiveInt(string) && Integer.parseInt(string) <= Vars.world.width());
+        y.setValidator((string) -> Strings.canParsePostiveInt(string) && Integer.parseInt(string) <= Vars.world.height());
+        table.add(x, new Label(","), y);
+        Image blockImage = new Image();
+        blockImage.update(() -> {
+            if(x.isValid() && y.isValid()){
+                Tile tile = Vars.world.tile(Integer.parseInt(x.getText()), Integer.parseInt(y.getText()));
+                blockImage.setDrawable(tile.block() == Blocks.air? tile.floor().icon(Cicon.small) : tile.block().icon(Cicon.small));
+            }
+        });
+         table.addImageButton(Icon.ok, () -> {
+             if(x.isValid() && y.isValid()){
+                 TransferEndpoint endpoint =  new TransferEndpoint(Vars.world.tile(Integer.parseInt(x.getText()), Integer.parseInt(x.getText())));
+                 if(isStart){
+                     start = endpoint;
+                 }else{
+                     end = endpoint;
+                 }
+             }
+         });
+        return table;
+    }
+
+    private Table buildPlayerPickup(boolean isStart){
+        Table table = new Table();
+        table.addImageButton(Icon.ok, () -> {
+            TransferEndpoint endpoint = new TransferEndpoint(Vars.player);
+            if(isStart){
+                start = endpoint;
+            }else{
+                end = endpoint;
+            }
+        });
+        return table;
+    }
+
+    private Table buildBlockTypePickup(boolean isStart){
+        Table table = new Table();
+        Table selector = new Table();
+        AtomicReference<Block> selected = new AtomicReference<>();
+        selected.set(null);
+        ItemSelection.buildTable(selector, Vars.content.blocks(), selected::get, selected::set);
+        table.add(selector);
+        table.addImageButton(Icon.ok, () -> {
+            if(selected.get() != null){
+                TransferEndpoint endpoint = new TransferEndpoint(selected.get());
+                if(isStart){
+                    start = endpoint;
+                }else{
+                    end = endpoint;
                 }
-            });
-            cont.add(blockImage);
-            cont.addButton("Next", () -> {
-                cont.removeChild(x);
-                cont.removeChild(y);
-                cont.removeChild(blockImage);
-                cont.removeChild(comma);
-            });
+            }
         });
-        typeSelector.row();
+        return table;
+    }
 
-        typeSelector.addButton("Block type", () -> {
-            cont.removeChild(typeSelector);
-        });
-        typeSelector.row();
-
-        typeSelector.addButton("Player items", () -> {
-            cont.removeChild(typeSelector);
-        });
-        cont.add(typeSelector);
+    public void build(){
         addCloseButton();
     }
 }
