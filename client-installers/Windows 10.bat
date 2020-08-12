c566a6caeaa70df4746c5fca0d4bd4b4217dfa53
buthed010203 <naguiar010203@gmail.com>
2020-08-12 18:28:09 -0400

    Create Windows 10.bat

---
+++ client-installers/Windows 10.bat
@@ -0,0 +1,19 @@
+@echo off
+:setInstall
+echo This script will (mostly) automatically install the client and will automatically keep it up to date. 
+echo Before continuing, please ensure that both git and OpenJDK 8 are installed.
+pause
+echo Please specify install location (something like C:\Desktop or D:\Games):
+set /p location=
+if not exist %location% (echo Error: %location% could not be found.&&goto setInstall)
+cd /d %location%
+echo Success: Install directory exists.
+echo Beginning client installation
+git clone https://github.com/blahblahbloopster/mindustry-client.git
+echo Success: Client installed successfully.
+echo Creating auto update script
+pause
+echo cd /d %location%\mindustry-client>foos-client.bat&&echo git pull>>foos-client.bat&&echo gradlew.bat desktop:run>>foos-client.bat
+pause
+echo Success: Auto update script created, to launch the client simply open %location%\foos-client.bat (you can move this script anywhere)
+pause
