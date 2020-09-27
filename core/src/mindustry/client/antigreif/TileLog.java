3acf0ca0e0c64be0306813fff6a880ed127be832
Max Leibowitz <403089@bsd48.org>
2020-09-26 22:47:51 -0700

    Added tile logs

--- core/src/mindustry/client/antigreif/TileLog.java
+++ core/src/mindustry/client/antigreif/TileLog.java
@@ -1,11 +1,34 @@
 package mindustry.client.antigreif;
 
+import arc.scene.ui.*;
+import arc.scene.ui.layout.*;
 import arc.struct.*;
+import mindustry.world.*;
 
 public class TileLog {
     public Seq<TileLogItem> log = new Seq<>();
+    public int x, y;
+
+    public TileLog(Tile tile){
+        x = tile.x;
+        y = tile.y;
+    }
 
     public void addItem(TileLogItem item) {
         log.add(item);
+    }
+
+    public Table toTable(){
+        Table table = new Table();
+        if (log.isEmpty()) {
+            table.add(new Label(String.format("No logs for %d,%d", x, y)));
+        } else{
+            table.add(new Label(String.format("Logs for %d,%d:", x, y)));
+            log.forEach(item -> {
+                table.row();
+                table.add(item.toElement());
+            });
+        }
+        return table;
     }
 }
