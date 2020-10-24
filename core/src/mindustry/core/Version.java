35c72c868062da71961d92a70ebaefa55e75bba8
Max Leibowitz <403089@bsd48.org>
2020-10-23 18:28:03 -0700

    AAA

--- core/src/mindustry/core/Version.java
+++ core/src/mindustry/core/Version.java
@@ -47,7 +47,7 @@ public class Version{
             build = Strings.canParseInt(map.get("build")) ? Integer.parseInt(map.get("build")) : -1;
         }
 
-        build = 109;
+        build = 110;
         type = "official";
         modifier = "alpha";
         number = 6;
