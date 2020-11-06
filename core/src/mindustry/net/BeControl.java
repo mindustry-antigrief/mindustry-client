46892d58f3c92db6f85636b5b58dd60f127452a5
buthed010203 <naguiar010203@gmail.com>
2020-11-06 18:40:48 -0500

    display

--- core/src/mindustry/net/BeControl.java
+++ core/src/mindustry/net/BeControl.java
@@ -94,7 +94,7 @@ public class BeControl{
 
         if(!headless){
             checkUpdates = false;
-            ui.showCustomConfirm(Core.bundle.format("be.update", "") + " " + updateBuild, "@be.update.confirm", "@ok", "@be.ignore", () -> {
+            ui.showCustomConfirm(Core.bundle.format("be.update (" + Version.clientBuild +")", "") + " " + updateBuild, "@be.update.confirm", "@ok", "@be.ignore", () -> {
                 try{
                     boolean[] cancel = {false};
                     float[] progress = {0};
