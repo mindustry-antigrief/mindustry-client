aa889d1cab6b5285f8eee36f79a4ed8de4649afc
Max Leibowitz <403089@bsd48.org>
2020-12-06 08:38:36 -0800

    Update to v121

--- core/src/mindustry/core/Version.java
+++ core/src/mindustry/core/Version.java
@@ -55,7 +55,7 @@ public class Version{
             build = Strings.canParseInt(map.get("build")) ? Integer.parseInt(map.get("build")) : -1;
         }
 
-        build = 120;
+        build = 121;
         revision = 0;
         type = "official";
         modifier = "release";
