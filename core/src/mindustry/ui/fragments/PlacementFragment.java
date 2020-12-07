fcc45397f6ce27b5838067ba407892c2d43e8be8
buthed010203 <naguiar010203@gmail.com>
2020-12-07 14:53:26 -0500

    im dumb sometimes

--- core/src/mindustry/ui/fragments/PlacementFragment.java
+++ core/src/mindustry/ui/fragments/PlacementFragment.java
@@ -450,7 +450,7 @@ public class PlacementFragment extends Fragment{
         //if the mouse intersects the table or the UI has the mouse, no hovering can occur
         if(Core.scene.hasMouse() || topTable.hit(v.x, v.y, false) != null) return null;
 
-        if (!Core.input.keyDown(Binding.invisible_units) && Core.input.keyDown(Binding.invisible_units) && !scene.hasField() && !scene.hasDialog() && !state.isPaused()) {
+        if (!Core.input.keyDown(Binding.invisible_units) && !scene.hasField() && !scene.hasDialog() && !state.isPaused()) {
             //check for a unit
             Unit unit = Units.closestOverlap(player.team(), Core.input.mouseWorldX(), Core.input.mouseWorldY(), 5f, u -> !u.isLocal());
             //if cursor has a unit, display it
