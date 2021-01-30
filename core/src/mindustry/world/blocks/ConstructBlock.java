ae5fb3b80698a3925dd623b11e0fe86e261799b2
buthed010203 <naguiar010203@gmail.com>
2021-01-29 23:11:23 -0500

    oops...

--- core/src/mindustry/world/blocks/ConstructBlock.java
+++ core/src/mindustry/world/blocks/ConstructBlock.java
@@ -94,7 +94,7 @@ public class ConstructBlock extends Block{
     public static void breakWarning(Tile tile, Block block, Unit builder){
         if (state.rules.infiniteResources || builder == null || !builder.isPlayer()) return; // Don't warn in sandbox for obvious reasons.
 
-        Seq<Block> warnBlocks = new Seq<>(new Block[]{Blocks.powerSource, Blocks.powerVoid, Blocks.itemSource, Blocks.itemVoid, Blocks.liquidSource, Blocks.liquidVoid, Blocks.duo}); // All blocks that shouldn't be broken. Note: Untested with multiblocks, likely to behave in a strange manner.
+        Seq<Block> warnBlocks = new Seq<>(new Block[]{Blocks.powerSource, Blocks.powerVoid, Blocks.itemSource, Blocks.itemVoid, Blocks.liquidSource, Blocks.liquidVoid}); // All blocks that shouldn't be broken. Note: Untested with multiblocks, likely to behave in a strange manner.
 
         if (warnBlocks.contains(block) && Time.timeSinceMillis(tile.lastBreakWarn) > 10_000) {
             AtomicInteger distance = new AtomicInteger(Integer.MAX_VALUE);
