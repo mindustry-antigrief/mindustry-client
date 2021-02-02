e72eda700f533268301d978261e9e71aed78b381
buthed010203 <naguiar010203@gmail.com>
2021-02-02 10:13:31 -0500

    fix coord pattern

--- core/src/mindustry/core/NetClient.java
+++ core/src/mindustry/core/NetClient.java
@@ -42,7 +42,7 @@ public class NetClient implements ApplicationListener{
     private static final float dataTimeout = 60 * 18;
     private static final float playerSyncTime = 2;
     public static final float viewScale = 2f;
-    private static final Pattern coordPattern = Pattern.compile("\\S*?(\\d+)(?:\\s|,|\\[[^]]*])+(\\d+)\\S*"); // This regex is a mess, it captures the coords into $1 and $2 while $0 contains all surrounding text as well.
+    private static final Pattern coordPattern = Pattern.compile("\\S*?(\\d+)(?:\\[[^]]*])*(?:\\s|,)+(?:\\[[^]]*])*(\\d+)\\S*"); // This regex is a mess, it captures the coords into $1 and $2 while $0 contains all surrounding text as well. https://regex101.com is the superior regex tester
 
     private long ping;
     private Interval timer = new Interval(5);
