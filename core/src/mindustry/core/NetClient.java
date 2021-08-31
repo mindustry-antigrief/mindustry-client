0bac6c4281af5716c57b77948d98fecd65e6b394
Max Leibowitz <403089@bsd48.org>
2021-08-31 12:44:35 -0700

    Maybe removed flickering

--- core/src/mindustry/core/NetClient.java
+++ core/src/mindustry/core/NetClient.java
@@ -9,6 +9,7 @@ import arc.math.geom.*;
 import arc.struct.*;
 import arc.util.*;
 import arc.util.CommandHandler.*;
+import arc.util.Timer;
 import arc.util.io.*;
 import arc.util.serialization.*;
 import mindustry.*;
