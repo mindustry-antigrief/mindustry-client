2a9335b6fbfbce7a82df6b56ec21a24f020a96bc
zxtej <83355748+zxtej@users.noreply.github.com>
2021-09-14 19:45:40 +0800

    freeze plans
    idk, i think it works. shift+right click to remove the plans from the queue and display them as frozen
    might add the ability to shift them around and remove them directly and stuff but eh.

--- core/src/mindustry/Vars.java
+++ core/src/mindustry/Vars.java
@@ -14,6 +14,7 @@ import mindustry.core.*;
 import mindustry.ctype.*;
 import mindustry.editor.*;
 import mindustry.entities.*;
+import mindustry.entities.units.BuildPlan;
 import mindustry.game.EventType.*;
 import mindustry.game.*;
 import mindustry.gen.*;
@@ -245,6 +246,7 @@ public class Vars implements Loadable{
 
     public static Player player;
     public static boolean drawCursors, wasDrawingCursors; // Client debug magic
+    public static Seq<BuildPlan> frozenPlans = new Seq<>();
 
     @Override
     public void loadAsync(){
