08588325849ffb1d398f91c34d1c1b0c8f343342
buthed010203 <naguiar010203@gmail.com>
2021-09-22 19:12:53 -0400

    Update flood nullifier range

--- core/src/mindustry/world/blocks/power/ImpactReactor.java
+++ core/src/mindustry/world/blocks/power/ImpactReactor.java
@@ -31,6 +31,7 @@ public class ImpactReactor extends PowerGenerator{
     public int explosionRadius = 23;
     public int explosionDamage = 1900;
     public Effect explodeEffect = Fx.impactReactorExplosion;
+    public int floodNullifierRange = 16 * tilesize;
 
     public Color plasma1 = Color.valueOf("ffd06b"), plasma2 = Color.valueOf("ff361b");
 
@@ -65,16 +66,16 @@ public class ImpactReactor extends PowerGenerator{
         super.drawPlace(x, y, rotation, valid);
         Drawf.dashCircle(x * tilesize + offset, y * tilesize + offset, explosionRadius * tilesize, Color.coral);
         if (UtilitiesKt.flood()) {
-            Drawf.dashCircle(x * tilesize + offset, y * tilesize + offset, 10 * tilesize, Color.orange);
-            indexer.eachBlock(null, x * tilesize + offset, y * tilesize + offset, 10 * tilesize, b -> b instanceof CoreBlock.CoreBuild, b -> Drawf.selected(b, Color.orange));
+            Drawf.dashCircle(x * tilesize + offset, y * tilesize + offset, floodNullifierRange, Color.orange);
+            indexer.eachBlock(null, x * tilesize + offset, y * tilesize + offset, floodNullifierRange, b -> b instanceof CoreBlock.CoreBuild, b -> Drawf.selected(b, Color.orange));
         }
     }
 
     @Override
     public void drawRequestConfigTop(BuildPlan req, Eachable<BuildPlan> list){
         if (UtilitiesKt.flood()) {
-            Drawf.dashCircle(req.drawx(), req.drawy(), 10 * tilesize, Color.orange);
-            indexer.eachBlock(null, req.drawx(), req.drawy(), 10 * tilesize, b -> b instanceof CoreBlock.CoreBuild, b -> Drawf.selected(b, Color.orange));
+            Drawf.dashCircle(req.drawx(), req.drawy(), floodNullifierRange, Color.orange);
+            indexer.eachBlock(null, req.drawx(), req.drawy(), floodNullifierRange, b -> b instanceof CoreBlock.CoreBuild, b -> Drawf.selected(b, Color.orange));
         }
         if (!settings.getBool("showreactors")) return;
         Drawf.dashCircle(req.drawx(), req.drawy(), explosionRadius * tilesize, Color.coral);
