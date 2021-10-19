f358898a02d702cfa4821d36cfba25c3c75f2662
zxtej <83355748+zxtej@users.noreply.github.com>
2021-10-19 21:16:47 +0800

    die frozen plans when reloading game

--- core/src/mindustry/core/Control.java
+++ core/src/mindustry/core/Control.java
@@ -98,6 +98,7 @@ public class Control implements ApplicationListener, Loadable{
 
         Events.on(ResetEvent.class, event -> {
             player.reset();
+            frozenPlans.clear();
 
             hiscore = false;
             saves.resetSave();
