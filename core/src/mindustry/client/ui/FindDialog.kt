a22070cafd7c6d52959156e46950ca7dd40cb1d8
Max Leibowitz <403089@bsd48.org>
2022-01-23 17:16:01 -0800

    Add count to ctrl f

--- core/src/mindustry/client/ui/FindDialog.kt
+++ core/src/mindustry/client/ui/FindDialog.kt
@@ -4,6 +4,7 @@ import arc.*
 import arc.input.*
 import arc.scene.ui.*
 import arc.scene.ui.layout.*
+import mindustry.Vars
 import mindustry.Vars.*
 import mindustry.client.*
 import mindustry.client.utils.*
@@ -28,6 +29,9 @@ object FindDialog : BaseDialog("@find") {
             imageTable.row()
             first = false
         }
+        val allyOnly = CheckBox("Ally Only?")
+        cont.add(allyOnly)
+        cont.row()
         cont.add(inputField)
         cont.row()
         cont.add(imageTable)
@@ -46,13 +50,28 @@ object FindDialog : BaseDialog("@find") {
             if (it == KeyCode.enter) {
                 if (guesses.isEmpty()) return@keyDown // Pasting an emoji will cause this to crash otherwise
                 val block = guesses[0]
-                val closest = Units.findAllyTile(player.team(), player.x, player.y, Float.MAX_VALUE / 2) { t -> t.block == block }
+                var closest: Tile? = null
+                var dst2 = Float.MAX_VALUE
+                var count = 0
+
+                for (t in world.tiles) {
+                    if (t.block() == block) {
+                        if (allyOnly.isChecked && t.team().isEnemy(player.team())) continue
+                        val d = t.dst2(player)
+                        if (d < dst2) {
+                            closest = t
+                            dst2 = d
+                            count++
+                        }
+                    }
+                }
+
                 if (closest == null) {
                     ui.chatfrag.addMessage("No ${block.localizedName} was found", ClientVars.user)
                 } else {
-                    ClientVars.lastSentPos.set(closest.x / tilesize, closest.y / tilesize)
+                    ClientVars.lastSentPos.set(closest.x.toFloat(), closest.y.toFloat())
                     // FINISHME: Make the line below use toasts similar to UnitPicker.java
-                    ui.chatfrag.addMessage("Found ${block.localizedName} at ${closest.x / tilesize},${closest.y / tilesize} (!go to go there)", ClientVars.user)
+                    ui.chatfrag.addMessage("Found ${block.localizedName} at ${closest.x},${closest.y}, !go to go there ($count found total)", ClientVars.user)
                 }
                 Core.app.post(this::hide)
             }
