1d3b372a68b419860ed5a636a7517ca1d73675a2
SB <72693226+StormyBytes@users.noreply.github.com>
2022-05-16 20:14:18 +0700

    Minepath improvements
    I have not tested any of this.

--- core/src/mindustry/client/navigation/MinePath.kt
+++ core/src/mindustry/client/navigation/MinePath.kt
@@ -13,6 +13,7 @@ import mindustry.type.*
 class MinePath @JvmOverloads constructor(var items: Seq<Item> = player.unit().type.mineItems, var cap: Int = Core.settings.getInt("minepathcap"), val newGame: Boolean = false) : Path() {
     private var lastItem: Item? = null // Last item mined
     private var timer = Interval()
+    private var coreIdle = false
 
     companion object {
         init {
@@ -33,8 +34,10 @@ class MinePath @JvmOverloads constructor(var items: Seq<Item> = player.unit().ty
         if (items.isEmpty) {
             items = player.unit().type.mineItems
             if (split.none { Strings.parseInt(it) > 0 }) player.sendMessage("client.path.miner.allinvalid".bundle())
+        } else if (cap >= 0) {
+            player.sendMessage(Core.bundle.format("client.path.miner.tobuild", items.joinToString(), if (cap == 0) "∞" else cap))
         } else {
-            player.sendMessage(Core.bundle.format("client.path.miner.mining", items.joinToString(), if (cap == 0) "∞" else cap))
+            player.sendMessage(Core.bundle.format("client.path.miner.toidle", items.joinToString(), player.closestCore().storageCapacity))
         }
     }
 
@@ -44,22 +47,48 @@ class MinePath @JvmOverloads constructor(var items: Seq<Item> = player.unit().ty
     override fun follow() {
         val core = player.closestCore() ?: return
         var item = items.min({ indexer.hasOre(it) && player.unit().canMine(it) }) { core.items[it].toFloat() } ?: return
-        if (lastItem != null && player.unit().canMine(lastItem) && core.items[lastItem] - core.items[item] < 100) item = lastItem!! // Scuffed, don't switch mining until there's a 100 item difference, prevents constant switching of mine target
+        val maxCap = if (cap <= 0) core.storageCapacity else core.storageCapacity.coerceAtMost(cap)
+        if (lastItem != null && player.unit().canMine(lastItem) && core.items[lastItem] - core.items[item] < 100 && core.items[lastItem] < maxCap) item = lastItem!! // Scuffed, don't switch mining until there's a 100 item difference, prevents constant switching of mine target
         lastItem = item
-        if (!newGame && cap < core.storageCapacity && core.items[item] >= core.storageCapacity || cap != 0 && core.items[item] > cap) {  // Auto switch to BuildPath when core is sufficiently full
-            player.sendMessage(Strings.format("[accent]Automatically switching to BuildPath as the core has @ items (this number can be changed in settings).", if (cap == 0) core.storageCapacity else cap))
+
+        if (!newGame && core.items[item] >= maxCap && cap >= 0) {  // Auto switch to BuildPath when core is sufficiently full
+            coreIdle = false
+            player.sendMessage(Core.bundle.format("client.path.miner.build", maxCap))
             Navigation.follow(BuildPath(items, cap))
         }
 
-        if (player.unit().maxAccepted(item) <= 1) { // drop off
+        // idle at core
+        if (coreIdle) {
+            if (!player.within(core, itemTransferRange - tilesize * 15)) goTo(core, itemTransferRange - tilesize * 15)
+
+            if (player.unit().hasItem()) player.unit().clearItem() // clear items to prepare for MinePath resumption
+
+            if (core.items[item] < maxCap / 2) {
+                player.sendMessage(Core.bundle.get("client.path.miner.resume"))
+                coreIdle = false
+            }
+
+            return
+        }
+
+        // go to core and transfer items
+        if (player.unit().maxAccepted(item) <= 1) {
             if (player.within(core, itemTransferRange - tilesize * 10) && timer[30f]) {
+                player.unit().mineTile = null
                 Call.transferInventory(player, core)
+
+                // idle at core if cap < 0 (never switch to build path)
+                if (core.items[item] >= maxCap && cap < 0) {
+                    player.sendMessage(Core.bundle.format("client.path.miner.idle", maxCap))
+                    coreIdle = true
+                }
             } else {
                 if (player.unit().type.canBoost) player.boosting = true
                 goTo(core, itemTransferRange - tilesize * 15)
             }
 
-        } else { // mine
+        // mine
+        } else {
             val tile = indexer.findClosestOre(player.unit(), item) // FINISHME: Ignore blocked tiles
             player.unit().mineTile = tile
             if (tile == null) return
