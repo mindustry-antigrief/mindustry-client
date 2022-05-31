1b3fe1cad4dc193ead6e32dba1d38799582c3989
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-05-31 08:58:52 +0530

    and that is why you don't auto merge

--- core/src/mindustry/net/BeControl.java
+++ core/src/mindustry/net/BeControl.java
@@ -201,7 +201,7 @@ public class BeControl{
                     args.addAll(System.getProperties().entrySet().stream().map(it -> "-D" + it).toArray(String[]::new));
                     if(OS.isMac) args.add("-XstartOnFirstThread");
                     args.addAll("-Dberestart", "-Dbecopy=" + fileDest.absolutePath(), "-jar", file.absolutePath(), "-firstThread");
-                    if(settings.getBool("autorestart")) Runtime.getRuntime().exec(args.toArray());
+                    if(Core.settings.getBool("autorestart")) Runtime.getRuntime().exec(args.toArray());
                     Core.app.exit();
                 }catch(IOException e){
                     dialog.cont.clearChildren();
