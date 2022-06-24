4783e8485877352702665b191cae2365ea0d5a6b
buthed010203 <naguiar010203@gmail.com>
2022-06-24 13:32:10 -0400

    Fix setting

--- core/src/mindustry/client/ClientLogic.kt
+++ core/src/mindustry/client/ClientLogic.kt
@@ -74,7 +74,7 @@ class ClientLogic {
             if (Core.settings.getBool("discordrpc")) Vars.platform.startDiscord()
             if (Core.settings.getBool("mobileui")) Vars.mobile = !Vars.mobile
             if (Core.settings.getBool("viruswarnings")) LExecutor.virusWarnings = true
-            if (Core.settings.getBool("autotransfer", false)) AutoTransfer.enabled = true
+            if (Core.settings.getBool("autotransfer")) AutoTransfer.enabled = true
 
             Autocomplete.autocompleters.add(BlockEmotes(), PlayerCompletion(), CommandCompletion())
 
