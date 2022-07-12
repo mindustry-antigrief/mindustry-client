8aee7802c042463a941a6f12bb9c3895018a0d0c
stormybytes <stormybytes@gmail.com>
2022-07-12 09:44:04 +0700

    Move procfind searching to clientthread
    Because regex slow

--- core/src/mindustry/client/utils/ProcessorFinder.java
+++ core/src/mindustry/client/utils/ProcessorFinder.java
@@ -1,13 +1,16 @@
 package mindustry.client.utils;
 
+import arc.Core;
 import arc.math.Mathf;
 import arc.math.geom.Vec2;
 import arc.struct.Seq;
 import kotlin.text.Regex;
 import mindustry.Vars;
+import mindustry.client.navigation.clientThread;
 import mindustry.gen.Building;
 import mindustry.graphics.Drawf;
 import mindustry.world.Tile;
+import mindustry.world.Tiles;
 import mindustry.world.blocks.logic.LogicBlock.LogicBuild;
 
 import java.util.HashSet;
@@ -26,43 +29,55 @@ public class ProcessorFinder {
         Seq<Building> builds = new Seq<>();
         player.team().data().buildings.getObjects(builds);
     
-        int matchCount = 0, processorCount = 0;
-        for (Building build : builds) {
-            if (build instanceof LogicBuild logicBuild ) {
-                for (Regex query : queries) {
-                    if (query.containsMatchIn((logicBuild.code))) {
-                        matchCount++;
-                        highlighted.add(logicBuild);
+        clientThread.post(() -> {
+            int matchCount = 0, processorCount = 0;
+            for (Building build : builds) {
+                if (build instanceof LogicBuild logicBuild ) {
+                    for (Regex query : queries) {
+                        if (query.containsMatchIn((logicBuild.code))) {
+                            matchCount++;
+                            highlighted.add(logicBuild);
+                        }
+                        processorCount++;
                     }
-                    processorCount++;
                 }
             }
-        }
     
-        // Log how many found
-        if (matchCount == 0) player.sendMessage("[accent]No matches found.");
-        else player.sendMessage(String.format("[accent]Found [coral]%d/%d[] [accent]matches.", matchCount, processorCount));
+            int finalMatchCount = matchCount;
+            int finalProcessorCount = processorCount;
+            Core.app.post(() -> {
+                if (finalMatchCount == 0) player.sendMessage("[accent]No matches found.");
+                else player.sendMessage(String.format("[accent]Found [coral]%d/%d[] [accent]matches.", finalMatchCount, finalProcessorCount));
+            });
+        });
+    
     }
     
     public static void searchAll() {
         highlighted.clear();
-        
-        int matchCount = 0, processorCount = 0;
-        for (Tile tile : Vars.world.tiles) {
-            if (tile.build instanceof LogicBuild logicBuild) {
-                for (Regex query : queries) {
-                    if (query.containsMatchIn((logicBuild.code))) {
-                        matchCount++;
-                        highlighted.add(logicBuild);
+    
+        Tiles tiles = Vars.world.tiles;
+        clientThread.post(() -> {
+            int matchCount = 0, processorCount = 0;
+            for (Tile tile : tiles) {
+                if (tile.build instanceof LogicBuild logicBuild) {
+                    for (Regex query : queries) {
+                        if (query.containsMatchIn((logicBuild.code))) {
+                            matchCount++;
+                            highlighted.add(logicBuild);
+                        }
+                        processorCount++;
                     }
-                    processorCount++;
                 }
             }
-        }
     
-        // Log how many found
-        if (matchCount == 0) player.sendMessage("[accent]No matches found.");
-        else player.sendMessage(String.format("[accent]Found [coral]%d/%d[] [accent]matches.", matchCount, processorCount));
+            int finalMatchCount = matchCount;
+            int finalProcessorCount = processorCount;
+            Core.app.post(() -> {
+                if (finalMatchCount == 0) player.sendMessage("[accent]No matches found.");
+                else player.sendMessage(String.format("[accent]Found [coral]%d/%d[] [accent]matches.", finalMatchCount, finalProcessorCount));
+            });
+        });
     }
 
     public static void list() {
