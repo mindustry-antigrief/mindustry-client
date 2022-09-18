3087c046f2865b758f618ce54f7b50ad3023bd3d
buthed010203 <naguiar010203@gmail.com>
2022-09-17 22:32:36 -0400

    - Fix auto update/mod restart for Windows users without a java install I hope

--- core/src/mindustry/Vars.java
+++ core/src/mindustry/Vars.java
@@ -322,8 +322,9 @@ public class Vars implements Loadable{
         drawCursors = settings.getBool("drawcursors");
         javaPath =
             new Fi(OS.prop("java.home")).child("bin/java").exists() ? new Fi(OS.prop("java.home")).child("bin/java").absolutePath() :
-            Core.files.local("jre/bin/java").exists() ? Core.files.local("jre/bin/java").absolutePath() :
-            "java";
+            Core.files.local("jre/bin/java").exists() ? Core.files.local("jre/bin/java").absolutePath() : // Unix
+            Core.files.local("jre/bin/java.exe").exists() ? Core.files.local("jre/bin/java.exe").absolutePath() : // Windows
+            "java"; // Fallback
 
         state = new GameState();
 
