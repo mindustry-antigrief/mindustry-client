577bf2d5e7af772ebcae624d6202324e3f24fd88
zxtej <83355748+zxtej@users.noreply.github.com>
2022-10-31 16:02:37 +0800

    Stop failing the tests.

--- core/src/mindustry/world/blocks/distribution/ItemBridge.java
+++ core/src/mindustry/world/blocks/distribution/ItemBridge.java
@@ -24,7 +24,7 @@ import static mindustry.Vars.*;
 
 public class ItemBridge extends Block{
     private static BuildPlan otherReq;
-    public static int phaseWeaveInterval = Math.max(Core.settings.getInt("weaveEndInterval", 2), 1);
+    public static int phaseWeaveInterval = Core.settings == null ? 1 : Math.max(Core.settings.getInt("weaveEndInterval", 2), 1);
 
     public final int timerCheckMoved = timers ++;
 
