a94acdcc9a9dc06dc14cb3d370664ac4c5af0684
zxtej <83355748+zxtej@users.noreply.github.com>
2022-11-03 22:25:49 +0800

    Search bar in settings
    Is it even called levenshtein at this point?

--- core/src/mindustry/client/utils/BiasedLevenshtein.java
+++ core/src/mindustry/client/utils/BiasedLevenshtein.java
@@ -33,4 +33,53 @@ public class BiasedLevenshtein {
     public static float biasedLevenshteinInsensitive(String x, String y) {
         return biasedLevenshtein(x.toLowerCase(), y.toLowerCase());
     }
+
+    public static float biasedLevenshteinLengthIndependent(String x, String y) {
+        if (x.length() > y.length()){
+            String temp = x;
+            x = y;
+            y = temp;
+        }
+        int xl = x.length(), yl = y.length();
+        int yw = yl + 1;
+        int[] dp = new int[(xl + 1) * yw];
+
+        for(int j=0; j <= yl; ++j){
+            dp[j] = 0; // Insertions at the beginning are free
+        }
+        for(int i=0, i1=0; i <= xl; ++i, i1 += yw){
+            dp[i1] = i;
+        }
+        for(int i = 1; i <= xl; i++){
+            for(int j = 1; j <= yl; j++){
+                int idx = i * yw + j;
+                dp[idx] = Math.min(dp[idx - yw - 1] + (x.charAt(i - 1) == y.charAt(j - 1) ? 0 : 1),
+                        Math.min(dp[idx - yw], dp[idx - 1]) + 1);
+            }
+        }
+
+        // startsWith
+        if(dp[yw * xl + xl] == 0) return 0f;
+        // Disregard insertions at the end - if it made it it made it
+        int output = xl;
+        for(int i = yw * xl; i < yw * xl + yl; ++i){
+            output = Math.min(output, dp[i]);
+        }
+        // contains
+        if (output == 0) return 0.5f; // Prefer startsWith
+        /* old code to sort by first occurrence, but that would be a bit weird
+        int i = yw * xl, stop = i + yl;
+        while(i < stop && output > 0){
+            output = Math.min(output, dp[i]);
+        }
+        if(output == 0){ // contains
+            return (float)(i - yw * xl) / yl;
+        }
+         */
+        return output;
+    }
+
+    public static float biasedLevenshteinLengthIndependentInsensitive(String x, String y) {
+        return biasedLevenshteinLengthIndependent(x.toLowerCase(), y.toLowerCase());
+    }
 }
--- core/src/mindustry/ui/dialogs/SettingsMenuDialog.java
+++ core/src/mindustry/ui/dialogs/SettingsMenuDialog.java
@@ -21,6 +21,7 @@ import arc.util.io.*;
 import mindustry.*;
 import mindustry.client.*;
 import mindustry.client.antigrief.*;
+import mindustry.client.utils.*;
 import mindustry.content.*;
 import mindustry.content.TechTree.*;
 import mindustry.core.*;
@@ -714,9 +715,14 @@ public class SettingsMenuDialog extends BaseDialog{
 
     public static class SettingsTable extends Table{
         protected Seq<Setting> list = new Seq<>();
+        private static Seq<Setting> listSorted = new Seq<>();
+        private String search = "";
+        private Table searchBarTable;
+        private TextField searchBar;
 
         public SettingsTable(){
             left();
+            makeSearchBar();
         }
 
         public Seq<Setting> getSettings(){
@@ -726,6 +732,17 @@ public class SettingsMenuDialog extends BaseDialog{
         public void pref(Setting setting){
             list.add(setting);
             rebuild();
+        }
+
+        private void makeSearchBar(){
+            searchBarTable = table(s -> {
+                s.left();
+                s.image(Icon.zoom);
+                searchBar = s.field(search, res -> {
+                    search = res;
+                    rebuild();
+                }).growX().get();
+            }).get();
         }
 
         public SliderSetting sliderPref(String name, int def, int min, int max, StringProcessor s){
@@ -777,10 +794,26 @@ public class SettingsMenuDialog extends BaseDialog{
         }
 
         public void rebuild(){
+            boolean hasFocus = searchBar.hasKeyboard();
             clearChildren();
+            // TODO inefficient. And rebuild() is also called every. single. setting.
+            add(searchBarTable).fillX().padBottom(4);
+            row();
 
-            for(Setting setting : list){
-                setting.add(this);
+            if(search.isEmpty()){
+                for(Setting setting : list){
+                    setting.add(this);
+                }
+            }else{
+                listSorted.selectFrom(list, s -> !(s instanceof Category));
+                listSorted.sort(Structs.comparingFloat(u ->
+                    u.title == null ? Float.POSITIVE_INFINITY :
+                            BiasedLevenshtein.biasedLevenshteinLengthIndependentInsensitive(search, Strings.stripColors(u.title))
+                        // Maybe if distance == length, do not include? But troublesome
+                ));
+                for(Setting setting : listSorted){
+                    setting.add(this);
+                }
             }
 
             button(bundle.get("settings.reset", "Reset to Defaults"), () -> {
@@ -790,6 +823,10 @@ public class SettingsMenuDialog extends BaseDialog{
                 }
                 rebuild();
             }).margin(14).width(240f).pad(6);
+
+            if(hasFocus){
+                searchBar.requestKeyboard();
+            }
         }
 
         public abstract static class Setting{
