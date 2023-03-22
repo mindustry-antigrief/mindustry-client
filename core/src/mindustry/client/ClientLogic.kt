2d62378fbf94a4043ed12522cfcd80197ef3eb60
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-03-22 10:49:45 +0530

    Make the id in leave message a click to copy button

--- core/src/mindustry/client/ClientLogic.kt
+++ core/src/mindustry/client/ClientLogic.kt
@@ -161,7 +161,9 @@ class ClientLogic {
             if (settings.getBool("clientjoinleave") && (ui.chatfrag.messages.isEmpty || !Strings.stripColors(ui.chatfrag.messages.first().message).equals("${Strings.stripColors(e.player.name)} has disconnected.")))
                 player.sendMessage(bundle.format("client.disconnected", e.player.name))
             
-            if (settings.getBool("showidinjoinleave", false)) player.sendMessage(bundle.format("client.disconnected.withid", e.player.id.toString()))
+            if (settings.getBool("showidinjoinleave", false))
+                ui.chatfrag.addMsg(bundle.format("client.disconnected.withid", e.player.id.toString()))
+                    .addButton(e.player.id.toString()) { app.setClipboardText(e.player.id.toString()) }
         }
 
         Events.on(GameOverEventClient::class.java) {
