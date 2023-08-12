bbcfb7797c38f36b24060a7152d90005effb2a47
buthed010203 <naguiar010203@gmail.com>
2023-08-12 12:08:30 -0400

    Fix brackets

--- core/src/mindustry/client/utils/AutoTransfer.kt
+++ core/src/mindustry/client/utils/AutoTransfer.kt
@@ -156,7 +156,7 @@ class AutoTransfer {
 
         Time.run(delay/2F) {
             if (item != null && core != null && player.within(core, itemTransferRange) && ratelimitRemaining > 1) {
-                if (held > 0 && item != player.unit().stack.item && (!net.server() || player.unit().stack.amount > 0) Call.transferInventory(player, core)
+                if (held > 0 && item != player.unit().stack.item && (!net.server() || player.unit().stack.amount > 0)) Call.transferInventory(player, core)
                 else if (held == 0 || item != player.unit().stack.item || counts[maxID] > held) Call.requestItem(player, core, item, Int.MAX_VALUE)
                 else ratelimitRemaining++ // Yes im this lazy
                 item = null
