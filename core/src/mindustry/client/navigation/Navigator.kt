0e34ca52f5755dee18c36e107796c5abb764ec8a
Jason <131086642+JasonP01@users.noreply.github.com>
2024-01-04 15:33:41 +0200

    remove cn pathfinding
    hope i only deleted the right parts

--- core/src/mindustry/client/navigation/Navigator.kt
+++ core/src/mindustry/client/navigation/Navigator.kt
@@ -88,19 +88,11 @@ abstract class Navigator {
         }
 
         //Consider respawning at a core
-        if (Time.timeSinceMillis(lastWp) > 3000 && player.team().cores().any()) {
-            if (map.size > 0) { // CN auto core tp is different as a plugin allows for some magic...
-                val closestCore = map.minByOrNull { it.value.dst2(end) }!!
-                if (player.dst2(closestCore.value) > buildingRange * buildingRange && player.dst2(end) > closestCore.value.dst2(end)) {
-                    lastWp = Time.millis() // Try again in 3s
-                    Call.sendChatMessage("/wp ${closestCore.key}")
-                }
-            } else if (
+        if (
                 player.unit().spawnedByCore &&
                 player.unit().stack.amount == 0 &&
                 (if(player.unit() is Payloadc) !(player.unit() as Payloadc).hasPayload() else true)
-            ) { // Everything that isn't CN
-                val bestCore = player.team().cores().min(Structs.comps(Structs.comparingInt { -it.block.size }, Structs.comparingFloat { it.dst2(end) }))
+            ) { val bestCore = player.team().cores().min(Structs.comps(Structs.comparingInt { -it.block.size }, Structs.comparingFloat { it.dst2(end) }))
                 if (player.dst2(bestCore) > buildingRange * buildingRange && player.dst2(end) > bestCore.dst2(end) && player.dst2(bestCore) > player.unit().speed() * player.unit().speed() * 24 * 24) { // don't try to move if we're already close to that core
                     lastWp = Time.millis() // Try again in 3s FINISHME: Add the Call to the config queue as it uses ratelimit, prevent running this again until 3s after last one is processed
                     if (ClientVars.ratelimitRemaining > 1) Call.buildingControlSelect(player, bestCore)
