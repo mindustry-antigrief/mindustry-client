21ac535429893554adf81ac4ee444603c14b550f
SByte <stormybytes@gmail.com>
2024-01-04 20:02:04 +0700

    h

--- core/src/mindustry/client/Commands.kt
+++ core/src/mindustry/client/Commands.kt
@@ -805,7 +805,17 @@ fun setupCommands() {
         }
     }
 
-    // Funny commands (if yk yk)
+    // Funny commands
+    register("hh [h]", "!") { args, player ->
+        if (!net.client()) return@register
+        val u = if (args.any()) findUnit(args[0]) else player.unit().type
+        val current = ui.join.lastHost ?: return@register
+        if (current.group == null) current.group = ui.join.communityHosts.find { it == current } ?.group ?: return@register
+        switchTo = ui.join.communityHosts.filterTo(arrayListOf<Any>()) { it.group == current.group && it != current && (it.version == Version.build || Version.build == -1) }.apply { add(current); add(u) }
+        val first = switchTo!!.removeFirst() as Host
+        NetClient.connect(first.address, first.port)
+    }
+
     if (OS.hasProp("policone")) {
         register("poli", "Spelling is hard. This will make sure you never forget how to spell the plural of poly, you're welcome.") { _, _ ->
             sendMessage("Unlike a roly-poly whose plural is roly-polies, the plural form of poly is polys. Please remember this, thanks! :)")
@@ -813,16 +823,6 @@ fun setupCommands() {
 
         register("silicone", "Spelling is hard. This will make sure you never forget how to spell silicon, you're welcome.") { _, _ ->
             sendMessage("Silicon is a naturally occurring chemical element, whereas silicone is a synthetic substance. They are not the same, please get it right!")
-        }
-
-        register("hh [h]", "!") { args, player ->
-            if (!net.client()) return@register
-            val u = if (args.any()) findUnit(args[0]) else player.unit().type
-            val current = ui.join.lastHost ?: return@register
-            if (current.group == null) current.group = ui.join.communityHosts.find { it == current } ?.group ?: return@register
-            switchTo = ui.join.communityHosts.filterTo(arrayListOf<Any>()) { it.group == current.group && it != current && (it.version == Version.build || Version.build == -1) }.apply { add(current); add(u) }
-            val first = switchTo!!.removeFirst() as Host
-            NetClient.connect(first.address, first.port)
         }
     }
 }
