a091eef715bdabac9690262bec7fa2a7d1801fe9
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2024-02-22 20:13:24 +0530

    Improve tilelog config viewer

--- core/src/mindustry/client/antigrief/TileState.kt
+++ core/src/mindustry/client/antigrief/TileState.kt
@@ -17,6 +17,8 @@ import mindustry.gen.*
 import mindustry.type.*
 import mindustry.world.*
 import mindustry.world.blocks.*
+import mindustry.world.blocks.defense.*
+import mindustry.world.blocks.logic.*
 import java.time.*
 import kotlin.math.*
 
@@ -104,7 +106,7 @@ class TileState {
         }))
         table.row()
 
-        if (configuration != null && (configuration as? Array<*>)?.isNotEmpty() == true) {
+        if (configuration != null) {
             table.label("config:")
             table.row()
             val config = configuration
@@ -117,6 +119,8 @@ class TileState {
 
                 config is Block -> table.image(config.uiIcon)
 
+                config is UnitType -> table.image(config.uiIcon)
+
                 config is Array<*> && config.getOrNull(0) is Point2 -> {
                     // shut up I promise it's an array of point2s
                     val array = config as Array<Point2>
@@ -124,8 +128,20 @@ class TileState {
                     table.button(Icon.copySmall) { Core.app.clipboardText = array.joinToString { "(${it.x + x}, ${it.y + y})" } }
                 }
 
-                else -> table.label(config.toString().ascii().capLength(20)).table.button(Icon.copy) { Core.app.clipboardText = config.toString() }
+                config is String -> table.add("Text config:\n${config}")
+
+                config is Boolean && block is Door -> table.add(if(config) "open" else "closed")
+
+                config is Boolean && block is SwitchBlock -> table.add(if(config) "on" else "off")
+
+                config is Point2 -> table.add("${config.x + x}, ${config.y + y} (${config.x}, ${config.y})")
+
+                config is ByteArray && block is LogicBlock -> table.add(LogicBlock.decompress(config))
+
+                else -> table.label(config.toString().ascii().capLength(20))
+
             }
+
         }
 
         return table
